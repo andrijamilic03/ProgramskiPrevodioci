11afbbdb1a7465dbbf518f0942ba5d8d9b68287f
Andrej Vujnic <147924315+Vujnic@users.noreply.github.com>
2024-11-16 10:30:44 +0100

    Delete PP directory

--- PP/src/main/java/rs/raf/ccc/ast/Statement.java
+++
@@ -1,9 +0,0 @@
-package rs.raf.ccc.ast;
-
-
-/** Base class for all statements.  */
-public abstract class Statement extends Tree {
-    public Statement(Location location) {
-        super(location);
-    }
-}
